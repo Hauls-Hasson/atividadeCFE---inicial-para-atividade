910ac46855de270f27468e86211731d8bd325825
Ismael Rodrigues de Sousa <58527984+Hauls-Hasson@users.noreply.github.com>
2023-11-30 02:17:20 -0300

    botão de voltar ao topo funcional

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 //efeito do botão voltar ao Topo
 
+    function topo()(
+        window.scrollTo(
+            {
+                top:0,
+                left:0,
+                behaivor:'smooth'
+            }
+        )
+    )
+
 //Validação de Login
+
+
 
 //Ativar alert no botão cadastrar
